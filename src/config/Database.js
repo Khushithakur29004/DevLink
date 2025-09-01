0ec90835547991fea988ebd4d3989dd4970e239c
khushi thakur <khushi29004@gmail.com>
2025-09-01 15:20:18 +0530

    updated DB password

--- src/config/Database.js
+++ src/config/Database.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose");
 
 const connectDB = async ()=>{
     await mongoose.connect(
-        "mongodb+srv://NamasteNode:PqVprWrF2xBV2iaH@namastenode.txanrey.mongodb.net/devTinder"
+        "mongodb+srv://NamasteNode:iYqyNwqZub5KICg4@namastenode.txanrey.mongodb.net/devTinder"
     );
 };
 
